a41e172d2f4343f4e6514b0f94fa53ccf3fb1e2d
Kevin <phillips.kevin@gmail.com>
2022-02-16 22:17:24 -0800

    Added keyboard with unguessed letters Created the_unguessed() void function Improved UI Updated print_word() to void function

--- wordle.py
+++ wordle.py
@@ -2,7 +2,10 @@ import random
 
 #answer = "pants"
 #library = ["pants", "siren", "dumpy", "clear", "proxy", "knoll", "moist", "junky", "burst", "octal", "rinse", "human","after", "pinto", "roach", "login", "under", "purse", "uncle", "crime", "couch", "flute", "lotus", "whale"]
-
+qwerty_one = "   q w e r t y u i o p"
+qwerty_two = "    a s d f g h j k l"
+qwerty_three = "      z x c v b n m"
+the_guesses = []
 wordlength = 5 #new variable that will later be chosen value by player; removed hardcoded references to '5'
 
 library = []
@@ -17,6 +20,21 @@ def get_answer(library):
     answer = library[index]
     return answer
 
+def the_unguessed(word):
+    global qwerty_one
+    global qwerty_two
+    global qwerty_three
+    for letter in word:
+        if letter in qwerty_one:
+            qwerty_one = qwerty_one.replace(letter, "_")
+        elif letter in qwerty_two:
+            qwerty_two = qwerty_two.replace(letter, "_")
+        elif letter in qwerty_three:
+            qwerty_three = qwerty_three.replace(letter, "_")
+    print qwerty_one
+    print qwerty_two
+    print qwerty_three
+
 def print_word(word):
     #format_word = "[" + word[0] + "][" + word[1] + "][" + word[2] + "][" + word[3] + "][" + word[4] + "]"
     #return format_word
@@ -25,7 +43,8 @@ def print_word(word):
     while i < len(word):
         format_word = format_word + "[" + word[i] + "]"
         i += 1
-    return format_word
+    print "   ",
+    print format_word
 	
 def compare_word(answer, guess):
     output_string = ""
@@ -50,18 +69,19 @@ def guess_word():
     #print (not guess.isalpha())
     while (not guess.isalpha() or len(guess) != wordlength):
         guess = raw_input("Guess a word: ")
-    print print_word(compare_word(answer, guess.lower()))
+    print_word(compare_word(answer, guess.lower()))
+    print "\nUnguessed:\n"
+    the_unguessed(guess)
     #guess = raw_input("Guess a word: ")
     #print print_word(compare_word(answer, guess.lower()))
 	
 answer = get_answer(library)
 #print answer
 
-print print_word("?????")
-
+print_word("?????")
 turn = 0
 while ( turn < ( wordlength + 1 ) ):
-    print "Turn", turn + 1
+    print "\nTurn", turn + 1
     guess_word()
     turn += 1
 
