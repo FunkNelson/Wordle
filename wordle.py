dbf629020267c9ddcd4c7ec4266648aa4b5ddc9e
Kevin <phillips.kevin@gmail.com>
2022-02-21 09:23:44 -0800

    Major update: - add functionality for 6 letter word play -- add arg_parser() function to take command line input for game wordlength -- add dictionary6.txt for 6-letter word library - update data structure of word list -- rename dictionary files -- move library creation into its own function -- remove trailing "\n" at end of answers -- library now a list of list: each item is ["1","FARTS"] where 1 means valid as answer -- update get_answer() function for new data structure --- terminate game if valid answer not found in 10 attempts - add approve_word() function: takes player feedback on reasonabless of answer -- update dictionary_file if word not liked

--- wordle.py
+++ wordle.py
@@ -1,28 +1,56 @@
-import random
-import os
-os.system('clear')
+import random, os, sys
+os.system('clear') #start fresh
 
-#answer = "pants"
-#library = ["pants", "siren", "dumpy", "clear", "proxy", "knoll", "moist", "junky", "burst", "octal", "rinse", "human","after", "pinto", "roach", "login", "under", "purse", "uncle", "crime", "couch", "flute", "lotus", "whale"]
+#-------------  Global variable declarations --------------
+turn = 0  #starts the game with turn counter at 0
+is_correct = False #true means the answer has been guessed
+wordlength = 5 #default
 qwerty_one = "   q w e r t y u i o p"
 qwerty_two = "    a s d f g h j k l"
 qwerty_three = "      z x c v b n m"
 the_guess_list = []
-wordlength = 5 #new variable that will later be chosen value by player; removed hardcoded references to '5'
-turn = 0  #starts the game with turn counter at 0
-is_correct = False #true means the answer has been guessed
-
+answer_index = 1000000
 library = []
+dictionary_file = ""
+#----------------------------------------------------------
 
-with open("fiveletters.txt") as libfile:
-    for line in libfile:
-        library.append( line.rstrip( "\n" ).lower() )
-libfile.close()
+#argument parser
+# - stage 1: python wordle.py <wordlength>
+# - stage 2: python wordle.py <wordlength> <gamelength>
+def arg_parser():
+    global wordlength
+    global dictionary_file
+    if len(sys.argv) > 1:
+        print "Found argument"
+        if sys.argv[1] == "6":
+            wordlength = 6
+            print "Wordlength for game is: " + str(wordlength)
+    dictionary_file = "dictionary" + str(wordlength) + ".txt"
 
+#build a list variable within game based on dictionary file
+def build_library():
+    global library
+    with open(dictionary_file) as libfile:
+        for line in libfile:
+            word_item = line.rstrip("\r\n").lower().split(",")
+            library.append(word_item)
+    libfile.close()
+
+#find a valid answer within the library
 def get_answer(library):
-    index = random.randint(0, len(library)-1)
-    answer = library[index]
-    return answer
+    global answer_index
+    i = 0 #try to find a valid answer 10 times
+    while i < 10:
+        #print "Try #" + str(i+1) + " to find a valid answer"
+        answer_index = random.randint(0, len(library)-1)
+        #format for library: [['1','AFTER'], ... , ['0','ZORRO']] where 1 is valid answer, 0 is not
+        #print "Found: " + library[answer_index][1]
+        if library[answer_index][0] == "1":
+            answer = library[answer_index][1]
+            return answer
+        i += 1
+    print "Did not find a valid answer in " + str(i) + " attempts - quitting."
+    quit()
 
 def print_logo():
     with open("gnomefarts_logo.txt") as logo:
@@ -31,6 +59,7 @@ def print_logo():
     print "\n\n"
     logo.close()
 
+#function to update visual qwerty keyboard as letters are guessed
 def update_the_unguessed(word):
     global qwerty_one
     global qwerty_two
@@ -43,6 +72,7 @@ def update_the_unguessed(word):
         elif letter in qwerty_three:
             qwerty_three = qwerty_three.replace(letter, "_")
 
+#function to print the visual qwerty keyboard of unguessed letters
 def print_the_unguessed():
     print "\nUnguessed:\n"
     print qwerty_one
@@ -50,16 +80,12 @@ def print_the_unguessed():
     print qwerty_three
 
 def print_word(word):
-    #format_word = "[" + word[0] + "][" + word[1] + "][" + word[2] + "][" + word[3] + "][" + word[4] + "]"
-    #return format_word
-    #format_word = ""
     format_word = "  | "
     i = 0
     while i < len(word):
-        #format_word = format_word + "[" + word[i] + "]"
         format_word = format_word + word[i] + " | "
         i += 1
-    print format_word,   #removed line break from this print
+    print format_word, #removed line break from this print
 
 def print_empty_word():
     empty_word = ""
@@ -75,6 +101,27 @@ def print_guess_history(guess_list):
         print_word(compare_word(answer, guess))
         print guess
 
+#function gets player feedback on validity/reasonableness of word and update dictionary file if not
+def approve_word(answer_index):
+    global library
+    answer = library[answer_index]
+    approved = ""
+    while (approved != "y" and approved != "n"):
+        approved = raw_input("Was this a reasonable word? (y/n): ").lower()
+    if approved == 'n':
+        print "Thank you for the feedback, removing [" + answer[1] + "] from potential answers list."
+        answer[0] = "0" #remove word from library list object
+        #remove word from dictionary file
+        with open(dictionary_file) as libfile:
+            data = libfile.readlines()
+        data[answer_index] = "0," + answer[1].upper() + "\r\n"
+        with open(dictionary_file, 'w') as libfile:
+            libfile.writelines(data)
+        libfile.close()
+    else:
+        print "Acknowledged, keeping [" + answer[1] + "] in list."
+
+#function that determines correctness of guess word letters and formats output
 def compare_word(answer, guess):
     output_string = ""
     i = 0
@@ -92,13 +139,18 @@ def compare_word(answer, guess):
         i += 1
     return output_string
 
+#function that takes player guess input, forces correctness/validity of guess
 def guess_word():
     guess = "0"
     while (not guess.isalpha() or len(guess) != wordlength):
         guess = raw_input("Guess a word: ").lower()
+        if not any(guess in sublib for sublib in library) and guess.isalpha() and len(guess) == wordlength:
+            print "Your guess, [" + guess + "] was not found in the dictionary."
+            guess = "0"
     the_guess_list.append(guess)
     update_the_unguessed(guess)
 
+#function for game turn loop
 def take_turn():
     global turn
     global is_correct
@@ -113,24 +165,26 @@ def take_turn():
     print "\n - Turn", turn + 1, " -"
     guess_word()
 
-    if the_guess_list[-1].strip() == answer.strip():
+    if the_guess_list[-1] == answer:
         is_correct = True
         print "\nHooray you are a winnar!!!\n"
 
     turn += 1
 
-    if turn == 6:
+    if turn == (wordlength + 1):
         os.system('clear')
         print_guess_history(the_guess_list)
     return 
 
 #---------  Game logic ----------------
-
+arg_parser()
+build_library()
 answer = get_answer(library)
 print_logo()
 
 while ( turn < ( wordlength + 1 ) and is_correct == False ):	
     take_turn()
 
-print_word(answer.strip())	
+print_word(answer)
 print "\n"
+approve_word(answer_index)
